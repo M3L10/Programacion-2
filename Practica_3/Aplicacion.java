07de7ba412b3785ecaa2067376a57f117e0df635
meliodas <ronall045@gmail.com>
2025-10-01 09:41:24 -0400

    Agregado archivo Aplicacion.java en Practica_3

---
+++ Practica_3/Aplicacion.java
@@ -0,0 +1,11 @@
+package Practica_3;
+
+public class Aplicacion {
+ public static void main(String[] args) {
+     JuegoAdivinaNumero viaNum = new JuegoAdivinaNumero(3);
+
+     viaNum.juega();
+
+     System.out.println("Gracias por jugar.");
+ }
+}
